5cb3267fc139352af3957fe3431c6d5d0c224845
Sander Stokseth Skjulsvik <rednas.1604@gmail.com>
2020-01-07 16:05:46 +0100

    moved location names into station and made Component its own table

--- db_setup/setup.sql
+++ db_setup/setup.sql
@@ -1,33 +1,31 @@
-DROP TABLE Station, Zone, Municipality, Area, Readings;
-
-CREATE TABLE Zone (
-    name VARCHAR(255) PRIMARY KEY
-);
-CREATE TABLE Municipality (
-    name VARCHAR(255) PRIMARY KEY
-);
-CREATE TABLE Area (
-    name VARCHAR(255) PRIMARY KEY
-);
+DROP TABLE Station, Reading, Component;
 
 CREATE TABLE Station (
     id SERIAL PRIMARY KEY,
     eoi VARCHAR(7) NOT NULL,
     latitude FLOAT(10),
     longitude FLOAT(10),
-    zone_name VARCHAR(255) REFERENCES Zone(name),
-    zone_municipality VARCHAR(255) REFERENCES Municipality(name),
-    zone_area VARCHAR(255) REFERENCES Area(name),
-    UNIQUE(eoi)
+    zone_name VARCHAR(255),
+    zone_municipality VARCHAR(255),
+    zone_area VARCHAR(255),
+    UNIQUE (eoi)
 );
 
-CREATE TABLE Readings(
+CREATE TABLE Component (
+    id SERIAL PRIMARY KEY,
+    component VARCHAR(255) NOT NULL,
+    unit VARCHAR(255)
+);
+
+CREATE TABLE Reading(
     id SERIAL PRIMARY KEY,
     eoi VARCHAR(7) NOT NULL REFERENCES Station(eoi),
     time_from TIME NOT NULL,
     time_to TIME NOT NULL,
     value FLOAT(12) NOT NULL,
     index INTEGER,
-    unit VARCHAR(50),
-    component VARCHAR(255) NOT NULL
+    id_component INTEGER REFERENCES Component(id),
+
+    UNIQUE (eoi, time_from, time_to, id_component)
 );
+
